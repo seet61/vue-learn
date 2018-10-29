2d066ccb680507ccb9984752ba8fbe8caff69445
seet61 <seet_seet@mail.ru>
2018-10-29 13:20:55 +0300

    lesson 3.2

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        count: 0
+        url: "",
+        cleanUrl: ""
     },
     methods: {
-        countUp: function () {
-            this.count += 1;
-        },
-        countDown: function () {
-            this.count -= 1;
+        cleanerUrl: function() {
+            this.cleanUrl = this.url.replace(/^https?:\/\//, '').replace(/\/$/, '');
         }
     }
 })
