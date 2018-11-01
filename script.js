cb4fc851a607cc8fe5774b6e6f45a6b140a4a950
seet61 <seet_seet@mail.ru>
2018-11-01 18:28:03 +0300

    lesson 6

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-Vue.component('book', {
-    template: 'books',
-    props: ['title, author', 'content']
-});
+
 
 new Vue ({
-    el: '#app',
+    el: '#demo',
     data: {
-        author: 'David Flanagan1111',
-        title: 'JavaScript the Guide',
-        content: 'COntent of this book'
+        show: false
     }
 })
