1a43ed8df2d3e31d8cc3827e474345bc2e948ab2
seet61 <seet_seet@mail.ru>
2018-11-07 15:00:22 +0300

    les 8

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const Home = {template: '<h1>Home</h1>'};
-const About = {template: '<h1>About</h1>'};
-const Blog = {template: '<h1>Blog</h1>'};
-
-const routes = [
-    {path: '/home', component: Home},
-    {path: '/about', component: About},
-    {path: '/blog', component: Blog}
-]
-
-const router = new VueRouter({
-    routes
-})
-
-const app = new Vue({
-    router
-}).$mount('#app')
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: "Task list",
+        items: [
+            {text: "First"},
+            {text: "Second"},
+            {text: "First"},
+            {text: "Second"}
+        ]
+    },
+    methods: {
+        addItem: function() {
+            var input = document.getElementById('itemForm');
+            if (input.value !== '') {
+                this.items.push({text: input.value});
+                input.value = "";
+            }
+        },
+        deleteItem: function(index) {
+            this.items.splice(index, 1);
+        }
+    }
+});
