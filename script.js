505485cc0e7c501506e1314bb3adceee477937fa
seet61 <seet_seet@mail.ru>
2018-11-07 14:31:52 +0300

    les 7

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const Home = {template: '<h1>Home</h1>'};
+const About = {template: '<h1>About</h1>'};
+const Blog = {template: '<h1>Blog</h1>'};
 
+const routes = [
+    {path: '/home', component: Home},
+    {path: '/about', component: About},
+    {path: '/blog', component: Blog}
+]
 
-new Vue ({
-    el: '#demo',
-    data: {
-        show: false
-    }
+const router = new VueRouter({
+    routes
 })
+
+const app = new Vue({
+    router
+}).$mount('#app')
